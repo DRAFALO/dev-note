a0dcec8ea3fded4c9304b9bcd1bfc30d2ffb62b7
tranduybau <trdbau@gmail.com>
2024-01-14 22:37:29 +0700

    [task] update header

---
+++ components/layout/NavBar/LangToggle/index.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+
+import { i18n } from "@/dictionaries/i18n-config";
+import { LocaleEnum, MappingLocaleEnum } from "@/types/locales";
+
+interface LangToggleProps {
+  lang: string;
+}
+
+function LangToggle(props: LangToggleProps) {
+  const { lang } = props;
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const onRenderLinkByLocale = (locale: LocaleEnum) => {
+    if (!pathname) return "/";
+    const segments = pathname.split("/");
+    segments[1] = locale;
+    return segments.join("/");
+  };
+
+  const onChangeLocale = (locale: LocaleEnum) => {
+    const link = onRenderLinkByLocale(locale);
+
+    router.push(link);
+  };
+
+  return (
+    <Select onValueChange={onChangeLocale}>
+      <SelectTrigger className="w-auto min-w-[6.25rem]">
+        {MappingLocaleEnum[lang as LocaleEnum]}
+      </SelectTrigger>
+
+      <SelectContent>
+        {i18n.locales.map((locale) => (
+          <SelectItem key={`locale-${locale}`} value={locale}>
+            {MappingLocaleEnum[locale]} {locale === lang && "✨"}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
+
+export default React.memo(LangToggle);
