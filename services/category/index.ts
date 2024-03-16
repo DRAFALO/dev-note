b82af4bd42bef4c735a89412f1fbd7c3a143d82e
tranduybau <trdbau@gmail.com>
2024-03-16 14:51:23 +0700

    [1YdNVYUU] Fix deploy vercel

--- services/category/index.ts
+++ services/category/index.ts
@@ -1,21 +1,40 @@
-import { APICategoryResponse } from "@/types/categories";
+import { APICategoryResponse, CategoryEntity } from "@/types/categories";
 
-import { PATHES } from "..";
+// import { PATHES } from "..";
 
-import { APICategorySchema } from "./schema";
+// import { APICategorySchema } from "./schema";
 
-export function fetchCategories() {
-  return fetch(process.env.URL + PATHES.CATEGORIES)
-    .then((res) => res.json() as Promise<APICategoryResponse>)
-    .then((data) => {
-      const validated = APICategorySchema.safeParse(data);
+// export function fetchCategories() {
+//   return fetch(process.env.URL + PATHES.CATEGORIES)
+//     .then((res) => res.json() as Promise<APICategoryResponse>)
+//     .then((data) => {
+//       const validated = APICategorySchema.safeParse(data);
+//
+//       if (validated.success) {
+//         return validated.data;
+//       }
+//
+//       // Call api to report error (not correct response)
+//
+//       return data;
+//     });
+// }
 
-      if (validated.success) {
-        return validated.data;
-      }
+export const categories: CategoryEntity[] = [
+  { name: "review", _id: "1213" },
+  { name: "javascript", _id: "1213" },
+  { name: "typescript", _id: "1213" },
+  { name: "react", _id: "1213" },
+  { name: "nextjs", _id: "1213" },
+  { name: "tailwindcss", _id: "1213" },
+  { name: "css", _id: "1213" },
+  { name: "html", _id: "1213" },
+  { name: "user experience", _id: "1213" },
+  { name: "state", _id: "1213" },
+  { name: "programming", _id: "1213" },
+  { name: "performance", _id: "1213" },
+];
 
-      // Call api to report error (not correct response)
-
-      return data;
-    });
+export function fetchCategories(): APICategoryResponse {
+  return { data: categories, page: 1, total: 10 };
 }
